d5dfcb193439042813cff6a7470e318e4cb79b7b
silent-brad <bradscottwhite@gmail.com>
2025-10-20 21:40:12 -0400

    made nim functional

--- nim/minigrep.nim
+++ nim/minigrep.nim
@@ -1,46 +1,46 @@
 #[
-  Minigrep in Nim
+  Minigrep in Nim (functional style)
 ]#
 
 import std/os, std/strutils, std/strformat
 
 type Line = tuple[index: int, line: string]
 
-proc get_matches(query: string, lines: seq[string]): seq[Line] =
-  var
-    matches: seq[Line]
-    i = 0
-  for line in lines:
-    if line.find(query) != -1:
-      matches.add((index: i, line: line))
-    i += 1
-  return matches
+func get_matches(query: string, lines: seq[string], i: int = 0): seq[Line] =
+  if i < lines.len():
+    if lines[i].find(query) != -1:
+      @[(index: i, line: lines[i])] & get_matches(query, lines, i + 1)
+    else:
+      get_matches(query, lines, i + 1)
+  else:
+    @[]
 
-proc print_match(match: Line) =
+func print_match(match: Line): string =
   let (i, line) = match
-  echo fmt"{i}: {line}"
+  fmt"{i}: {line}"
 
-proc print_matches(matches: seq[Line]) =
-  for match in matches:
-    print_match(match)
+func print_matches(matches: seq[Line], i: int = 0): seq[string] =
+  if i < matches.len():
+    print_match(matches[i]) & print_matches(matches, i + 1)
+  else:
+    @[]
 
-proc grep(query: string, filename: string) =
-  let file = open(filename, fmRead)
+proc grep(query: string, file: File): string =
   if file != nil:
     let lines = file.read_all().split_lines()
     let matches = get_matches(query, lines)
     file.close()
     if matches.len() != 0:
-      print_matches(matches)
+      print_matches(matches).join("\n")
     else:
-      echo "No matches found"
+      "No matches found"
   else:
-    echo "File not found"
+    "File not found"
 
-proc minigrep(query: string, filename: string) =
-  if filename == "" and query == "":
-    echo "Usage: minigrep <query> <filename>"
-  else:
-    grep(query, filename)
-
-minigrep(param_str(1), param_str(2))
+let
+  query = param_str(1)
+  filename = param_str(2)
+if filename == "" and query == "":
+  echo "Usage: minigrep <query> <filename>"
+else:
+  echo grep(query, open(filename, fm_read))
